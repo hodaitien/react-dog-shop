06070e256edc66c37de02d54307cb64328c9092a
Tien Ho <hodaitien972019@gmail.com>
2022-01-18 20:50:54 +0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+function App() {
+  return (
+      <Router>
+        <div className="page-container">
+            <Routes>
+                <Route>
+                    
+                </Route>
+            </Routes>
+        </div>
+      </Router>
+  );
+}
+
+export default App;
