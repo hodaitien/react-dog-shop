77955b1c065b51efb313e32d517e75043e1f5d70
Tien Ho <hodaitien972019@gmail.com>
2022-01-19 17:10:40 +0700

    create dogs shop by useContext, useNavigate...

---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import "./home.css";
+
+const Home = () => {
+    return ( 
+        <>
+            <header>
+                Welcome to my pet shop
+            </header>
+        </>
+    );
+}
+ 
+export default Home;
